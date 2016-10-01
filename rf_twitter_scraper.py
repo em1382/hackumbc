c4639b6fcd295e35c10c8d1d56d4566f8b30623a
Ellis Madagan <em1382@messiah.edu>
2016-10-01 14:30:28 -0400

    Updated.

---
+++ rf_twitter_scraper.py
@@ -0,0 +1,11 @@
+# This program will form the building block for our HackUMBC project.
+# Authors:
+# - Ellis Madagan
+# - James Gelok
+# - Ben Underwood
+# - Justin Kim
+def main():
+    
+
+if __name__ == "__main__":
+    main()
